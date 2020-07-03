8c3a42c7c94c9656d481e1ca6ce9594b095cbba6
Bhavya2025 <66558143+Bhavya2025@users.noreply.github.com>
2020-07-03 17:10:10 +0530

    Add files via upload

---
+++ Hacker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu("Hello bhavya");
+        print ("Hello"+"World");
+    }
+void ShowMainMenu(string greeting){
+    Terminal.ClearScreen();
+    Terminal.WriteLine(greeting);
+    Terminal.WriteLine("What do u want to hack into?");
+    Terminal.WriteLine("Press 1 for the School");
+    Terminal.WriteLine("Press 2 for your city bank$Vault");
+    Terminal.WriteLine("Press 3 for NASA headQuaters");
+    Terminal.WriteLine("Enter your selection:");
+}
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
